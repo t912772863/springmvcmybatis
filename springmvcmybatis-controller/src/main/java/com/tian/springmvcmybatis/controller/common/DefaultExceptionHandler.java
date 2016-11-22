711986a1bf94859947cea830f0aa744d1e352df2
t912772863 <912772863@qq.com>
2016-11-22 15:02:44 +0800

    统一异常处理类

---
+++ springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/common/DefaultExceptionHandler.java
@@ -0,0 +1,41 @@
+package com.tian.springmvcmybatis.controller.common;
+
+import com.tian.springmvcmybatis.service.common.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 统一的异常返回处理
+ * Created by Administrator on 2016/11/22 0022.
+ */
+public class DefaultExceptionHandler implements HandlerExceptionResolver{
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object o, Exception e) {
+        ModelAndView mv = new ModelAndView();
+        //  使用response返回
+        response.setStatus(HttpStatus.OK.value()); //设置状态码
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE); //设置ContentType
+        response.setCharacterEncoding("UTF-8"); //避免乱码
+        response.setHeader("Cache-Control", "no-cache, must-revalidate");
+
+
+        try{
+            e.printStackTrace();
+            if(e instanceof BusinessException){
+                BusinessException be = (BusinessException)e;
+                // 如果是我们返回的业务异常则进行封装
+                response.getWriter().write("{\"code\":"+be.getErrorCode()+",\"message\":\""+be.getErrorMessage()+"\"}");
+            }else{
+                //返回统一内部错误
+                response.getWriter().write("{\"code\":500,\"message\":\""+e.getClass().getName()+"\"}");
+            }
+        }catch (Exception e2){
+            e2.printStackTrace();
+        }
+        return mv;
+    }
+}

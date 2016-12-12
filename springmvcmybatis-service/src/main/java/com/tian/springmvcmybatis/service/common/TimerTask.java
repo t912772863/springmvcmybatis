ea8f56847d1fc4690b0b881f5c76fa4c3c950727
t912772863 <912772863@qq.com>
2016-12-12 15:39:48 +0800

    测试一下定时任务是否是单独启动线程运行的

--- springmvcmybatis-service/src/main/java/com/tian/springmvcmybatis/service/common/TimerTask.java
+++ springmvcmybatis-service/src/main/java/com/tian/springmvcmybatis/service/common/TimerTask.java
@@ -11,6 +11,15 @@ import org.springframework.stereotype.Component;
 public class TimerTask {
     @Scheduled(cron = "0 0/1 * * * ?")
     public void testTimerTask(){
-        System.out.println("--------------定时任务运行了");
+        System.out.println("--------------定时任务运行开始了");
+        try {
+            // 等待的过程中,不影响其它方法的调用,说明定时任务是另起的线程.
+            // 值得注意的一点是IDEA编辑,如果有断点,其它所有的线程也会等待.
+            // 这个可能与IDEA工具本身的实现有关系.像eclipse就不会等待
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("--------------定时任务运行结束了");
     }
 }

e6dde0e7d35a421068c4744437b84132b12c6909
tianxiong <tianxiong@gongxiangcloud.com>
2016-12-26 21:35:51 +0800

    工具类修改,去掉不必要的引用包

--- springmvcmybatis-service/src/main/java/com/tian/springmvcmybatis/service/common/util/ObjectUtil.java
+++ springmvcmybatis-service/src/main/java/com/tian/springmvcmybatis/service/common/util/ObjectUtil.java
@@ -4,42 +4,11 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.Method;
-
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * Created by Administrator on 2016/12/14 0014.
  */
-public class ObjectUtil extends org.apache.commons.lang3.ObjectUtils {
-    /**
-     * 注解到对象复制，只复制能匹配上的方法。
-     *
-     * @param annotation
-     * @param object
-     */
-    public static void annotationToObject(Object annotation, Object object) {
-        if (annotation != null) {
-            Class<?> annotationClass = annotation.getClass();
-            Class<?> objectClass = object.getClass();
-            for (Method m : objectClass.getMethods()) {
-                if (StringUtils.startsWith(m.getName(), "set")) {
-                    try {
-                        String s = StringUtils.uncapitalize(StringUtils.substring(m.getName(), 3));
-                        Object obj = annotationClass.getMethod(s).invoke(annotation);
-                        if (obj != null && !"".equals(obj.toString())) {
-                            if (object == null) {
-                                object = objectClass.newInstance();
-                            }
-                            m.invoke(object, obj);
-                        }
-                    } catch (Exception e) {
-                        // 忽略所有设置失败方法
-                    }
-                }
-            }
-        }
-    }
+public class ObjectUtil {
 
     /**
      * 序列化对象

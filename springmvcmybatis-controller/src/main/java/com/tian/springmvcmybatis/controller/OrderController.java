23e634abb582c66dfbc9e7f01e2b071da41525ed
t912772863 <912772863@qq.com>
2016-12-29 10:54:20 +0800

    浏览器下载文件的时候,ie中文乱码问题

--- springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/OrderController.java
+++ springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/OrderController.java
@@ -1,10 +1,10 @@
 package com.tian.springmvcmybatis.controller;
 
 import com.tian.springmvcmybatis.controller.common.ResponseData;
-import com.tian.springmvcmybatis.service.common.util.DocumentExport;
 import com.tian.springmvcmybatis.dao.common.validation.NotNull;
 import com.tian.springmvcmybatis.dao.entity.Order;
 import com.tian.springmvcmybatis.service.IOrderService;
+import com.tian.springmvcmybatis.service.common.util.DocumentExport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URLEncoder;
 import java.util.List;
 
 /**
@@ -60,8 +61,20 @@ public class OrderController extends BaseController{
         // 查询出数据
         List<Order> list = orderService.queryOrderByRule(getSessionUser(request).getId());
         OutputStream out = response.getOutputStream();
+
+        String fileName = "演示文件.xls";
+        // 区分是否为ie浏览器,解决中文乱码问题
+        String header = request.getHeader("User-Agent").toUpperCase();
+        if (header.contains("MSIE") || header.contains("TRIDENT") || header.contains("EDGE")) {
+            fileName = URLEncoder.encode(fileName, "utf-8");
+            fileName = fileName.replace("+", "%20");    //IE下载文件名空格变+号问题
+        } else {
+            fileName = new String(fileName.getBytes("utf-8"), "ISO8859-1");
+        }
+
         // 设置响应头,
-        response.setHeader("content-disposition", "attachment;filename=" + new String("demo.xlsx".getBytes("GB2312"), "ISO8859-1"));
+        response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+        response.setContentType("application/octet-stream");
         //写入excel文件中
         String[] strings = new String[]{"订单ID","第三方订单ID","用户ID","总金额(单位分)","备注","订单状态","创建时间","更新时间","数据状态"};
         DocumentExport.exportExcel("title",strings,list,out,"yyyy-MM-dd HH:mm:ss");

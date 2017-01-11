b529eacaaf56d175904c6bfc7d7c37957d3c35b8
t912772863 <912772863@qq.com>
2017-01-11 10:50:48 +0800

    添加图片表初始化sql

--- document/springmvc_mybatis.sql
+++ document/springmvc_mybatis.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50528
 File Encoding         : 65001
 
-Date: 2017-01-10 14:42:42
+Date: 2017-01-11 10:49:59
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -39,6 +39,27 @@ CREATE TABLE `activity` (
 -- ----------------------------
 INSERT INTO `activity` VALUES ('1', 'test1', 'aaaaa', 'aaaaaa', '2016-12-15 19:11:52', '2016-12-16 11:38:57', '2016-12-16 11:40:00', '2016-12-16 15:12:25', '2016-12-16 15:12:24', '3', '1');
 INSERT INTO `activity` VALUES ('2', 'test2', 'bbbb', 'bbbbb', '2016-12-15 19:11:52', '2016-12-16 11:48:57', '2016-12-15 11:53:18', '2016-12-16 17:59:25', '2016-12-16 17:59:25', '3', '1');
+
+-- ----------------------------
+-- Table structure for image
+-- ----------------------------
+DROP TABLE IF EXISTS `image`;
+CREATE TABLE `image` (
+  `ID` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '系统图片ID',
+  `DATA_ID` bigint(20) NOT NULL COMMENT '所属记录的主键',
+  `TABLE_NAME` varchar(50) NOT NULL COMMENT '属于系统哪张表',
+  `EXTEND_TYPE` varchar(50) DEFAULT NULL COMMENT '属于表中的哪个字段(当一第记录中只有一类图片时,可以为空.如果一条记录有多种类型图片:活动地点图片,活动宣传图片)',
+  `URL` varchar(100) NOT NULL COMMENT '图片的相对路径',
+  `CREATE_TIME` datetime NOT NULL COMMENT '创建时间',
+  `UPDATE_TIME` datetime DEFAULT NULL COMMENT '更新时间',
+  `STATUS` int(11) NOT NULL DEFAULT '1' COMMENT '数据状态:-1删除,   1正常',
+  PRIMARY KEY (`ID`),
+  KEY `表名_ID联合索引` (`DATA_ID`,`TABLE_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='系统图片表';
+
+-- ----------------------------
+-- Records of image
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for order

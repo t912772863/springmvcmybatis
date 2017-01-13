bb8f109ba547bd25897c19f40156304c93220b0c
t912772863 <912772863@qq.com>
2017-01-13 18:06:38 +0800

    ..........

--- springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/RoleController.java
+++ springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/RoleController.java
@@ -1,7 +1,7 @@
 package com.tian.springmvcmybatis.controller;
 
 import com.tian.springmvcmybatis.controller.common.ResponseData;
-import com.tian.springmvcmybatis.dao.common.validation.NotNull;
+import com.tian.springmvcmybatis.dao.common.validation.Number;
 import com.tian.springmvcmybatis.service.IRoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -25,7 +25,7 @@ public class RoleController extends BaseController{
      */
     @RequestMapping("query_role_by_id")
     @ResponseBody
-    public ResponseData queryRoleById(@NotNull Long id){
+    public ResponseData queryRoleById(@Number Long id){
         return successData.setData(roleService.queryRoleById(id));
     }
 

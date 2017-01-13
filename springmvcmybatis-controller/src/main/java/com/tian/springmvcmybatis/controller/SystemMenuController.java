ac99508d3bc0898339869c9c3102da0e186c6d16
t912772863 <912772863@qq.com>
2017-01-13 17:43:17 +0800

    删除多数据源,因为之前没有处理好多数据源事务问题,以后再学习这个

--- springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/SystemMenuController.java
+++ springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/SystemMenuController.java
@@ -2,9 +2,7 @@ package com.tian.springmvcmybatis.controller;
 
 import com.tian.springmvcmybatis.controller.common.ResponseData;
 import com.tian.springmvcmybatis.dao.entity.SystemMenu;
-import com.tian.springmvcmybatis.service.ISystemMenuService;
 import com.tian.springmvcmybatis.service.common.SystemInitData;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,9 +16,6 @@ import java.util.List;
 @Controller
 @RequestMapping("system_menu")
 public class SystemMenuController extends BaseController{
-    @Autowired
-    private ISystemMenuService systemMenuService;
-
     /**
      * 查询系统所有的菜单
      * @return

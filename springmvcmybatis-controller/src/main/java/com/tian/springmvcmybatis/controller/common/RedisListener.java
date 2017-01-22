bc0b0c8412d5f688f043baab6f2151818fc840f7
t912772863 <912772863@qq.com>
2017-01-22 17:55:28 +0800

    1.添加了一个二维码的工具类 2.实现了发送消息的定时发送功能(后期补上一个方法就可以实现暂停等功能,还可以再加上修改数据状态的逻辑) 3.一个redis键过期事件的配置文档

--- springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/common/RedisListener.java
+++ springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/common/RedisListener.java
@@ -9,9 +9,6 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
 
 import javax.annotation.PostConstruct;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * redis中key事件监听器
@@ -20,7 +17,6 @@ import java.util.concurrent.LinkedBlockingQueue;
 @Component
 public class RedisListener extends JedisPubSub {
 
-    private static final ExecutorService executor = Executors.newSingleThreadExecutor();
     @Autowired
     IActivityService activityService;
 
@@ -64,25 +60,36 @@ public class RedisListener extends JedisPubSub {
          System.out.println(pattern + "=" + subscribedChannels);
     }
 
-    // 取得按表达式的方式订阅的消息后的处理
+    /**
+     *  取得按表达式的方式订阅的消息后的处理
+     *
+     *  注意的是该方法内如果出现异常,一定要捕捉,不能拋出,否则会导致监视键过期的线程死掉
+     * @param pattern
+     * @param channel
+     * @param message
+     */
     public void onPMessage(String pattern, String channel, String message) {
-        // 这里的message就是设置的key
-        if(message.startsWith("sendMessage")){
-            // 发送消息任务开始
+        try{
+            System.out.println("------------------>" +message);
+            // 这里的message就是设置的key
+            if(message.startsWith("sendMessage")){
+                // 发送消息任务开始
+                TimerTask.doSend(message);
+            }else {
+                // 活动状态变迁
+                String[] strArr = message.split("_");
+                Activity activity = new Activity();
+                activity.setId(Long.parseLong(strArr[0]));
+                activity.setActivityStatus(Integer.parseInt(strArr[1]));
 
-            // 拿到要开始发送的号码集合
-            LinkedBlockingQueue queue = TimerTask.sendMessageMap.get(message);
+                // 更新状态
+                activityService.updateActivityById(activity);
+                System.out.println(pattern + "=" + channel + "=" + message);
+            }
 
-        }else {
-            // 活动状态变迁
-            String[] strArr = message.split("_");
-            Activity activity = new Activity();
-            activity.setId(Long.parseLong(strArr[0]));
-            activity.setActivityStatus(Integer.parseInt(strArr[1]));
-
-            // 更新状态
-            activityService.updateActivityById(activity);
-            System.out.println(pattern + "=" + channel + "=" + message);
+        }catch (Exception e){
+            e.printStackTrace();
+            // 处理失败的逻辑,一定不要拋出新的异常
         }
 
     }
--- springmvcmybatis-service/src/main/java/com/tian/springmvcmybatis/service/common/TimerTask.java
+++ springmvcmybatis-service/src/main/java/com/tian/springmvcmybatis/service/common/TimerTask.java
@@ -1,7 +1,6 @@
 package com.tian.springmvcmybatis.service.common;
 
 import com.tian.springmvcmybatis.dao.dto.ActivityDto;
-import com.tian.springmvcmybatis.dao.entity.File;
 import com.tian.springmvcmybatis.dao.entity.SendMessage;
 import com.tian.springmvcmybatis.service.IActivityService;
 import com.tian.springmvcmybatis.service.IFileService;
@@ -10,7 +9,6 @@ import com.tian.springmvcmybatis.service.common.util.DateUtil;
 import com.tian.springmvcmybatis.service.common.util.DocumentUtil;
 import com.tian.springmvcmybatis.service.common.util.JedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 
@@ -19,6 +17,8 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -28,7 +28,18 @@ import java.util.concurrent.LinkedBlockingQueue;
 @Component
 public class TimerTask {
 
-    public static final Map<String,LinkedBlockingQueue> sendMessageMap = new HashMap<String, LinkedBlockingQueue>();
+    /**
+     * 发送信息任务的数据
+     */
+    public static final Map<String,Runnable> sendMessageMap = new HashMap<String, Runnable>();
+    /**
+     * 发送信息任务的开关
+     */
+    public static final Map<String,Boolean> switchMap = new HashMap<String, Boolean>();
+    /**
+     * 固定大小为5的线程池来处理发送消息任务
+     */
+    public static final ExecutorService executor = Executors.newFixedThreadPool(5);
     @Autowired
     IActivityService activityService;
     @Autowired
@@ -58,7 +69,8 @@ public class TimerTask {
     /**
      * 把快要到定时变更活动状态的数据加入到内存中,通过redis的键过期通知事件,调用方法.
      */
-    @Scheduled(cron = "0 0/59 * * * ?")
+//    @Scheduled(cron = "0 0/59 * * * ?")
+//    @Scheduled(cron = "0 0/1 * * * ?")
     public void updateActivityStatus(){
         // 从数据库中查询时间在60分钟后就要到的
         String startTime = DateUtil.getDateAfterHour(0,"yyyy-MM-dd HH:mm:ss");
@@ -84,7 +96,7 @@ public class TimerTask {
     /**
      * 加载发送消息的号码集
      */
-    @Scheduled(cron = "0 0/59 * * * ?")
+//    @Scheduled(cron = "0 0/1 * * * ?")
     public void loadSendMessageNumber() throws IOException {
         // 从数据库中查询时间在60分钟后就要到的
         String startTime = DateUtil.getDateAfterHour(0,"yyyy-MM-dd HH:mm:ss");
@@ -96,7 +108,6 @@ public class TimerTask {
 
         // 把每个任务的id和对应该的文件读取出来关联
         List<String> fileList = null;
-        java.io.File file = null;
         Map<String,Object> phoneMap = null;
 
         // 当前时间毫秒值
@@ -105,20 +116,71 @@ public class TimerTask {
             // 根据发送消息ID查询出对应的excel文件,并把文件中的手机号读取出来放入队列中,
             fileList = fileService.queryFileByTableNameAndDataId("send_message",s.getId());
             // 因为任务和文件是一对一的,所以直接取第一个就可以了
-            phoneMap = DocumentUtil.readXlsx(fileList.get(0));
-            LinkedBlockingQueue queue = new LinkedBlockingQueue((List)phoneMap.get("list"));
-            sendMessageMap.put("sendMessage_"+s.getId(),queue);
+            String filePath = fileList.get(0);
+            // 数据库存的是页面的访问路径,这里要转换成真实的物理路径
+            filePath = "D:/tomcat/tomcatFile/"+filePath.substring(5);
+            phoneMap = DocumentUtil.readXlsx(filePath);
+            final LinkedBlockingQueue queue = new LinkedBlockingQueue((List)phoneMap.get("list"));
+
+            /*
+            创建一个任务runnable,把任务交给线程池去做.所以要用到匿名内部类
+            但是要注意一点的是,匿名内部类是没有带参数的构造方法的,而实际业务中,有参数要传递
+            两种方式解决这个问题:
+            1. 新建一个类实现runnable接口,在这个类中添加有参构造方法,把参数传进来(但是这个方法有一个缺点就是
+                会造成类的膨胀,因为类只会用一次)
+            2. 还是用匿名内部类,不过除了必要的run方法以外,如下,还添加了一个自定义的方法,用来获取参数,并赋值给成员变量,
+               这样run方法内,就可以调用参数了.
+             */
+            Runnable runnable = new Runnable() {
+                private String id;
+                private LinkedBlockingQueue queue;
+                public void run() {
+                    System.out.println("------------->开始发送消息了");
+                    while(TimerTask.switchMap.get(id)){
+                        Object obj = queue.poll();
+                        if(obj == null){
+                            TimerTask.switchMap.put(id,new Boolean(false));
+                            return;
+                        }
+                        System.out.println(obj+"发送成功");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                // 匿名内部类用来和外部沟通参数的方法
+                private Runnable getParams(String id,LinkedBlockingQueue queue){
+                    this.id = id;
+                    this.queue = queue;
+                    return this;
+                }
+            }.getParams("sendMessage_"+s.getId(),queue);
+            // 任务数据
+            sendMessageMap.put("sendMessage_"+s.getId(),runnable);
+            // 任务开关
+            switchMap.put("sendMessage_"+s.getId(),new Boolean(true));
 
             // 针对每一个发送消息任务,添加倒计时到redis中
             // 拼接自定义的key
             String key = "sendMessage_"+s.getId();
             // 把redis库切换到1号库
             Jedis jedis = JedisUtil.getResource();
-            jedis.select(1);
             // 把值放进去,同时设定一个过期时间
             int time = (int)(s.getSendTime().getTime()-current)/1000;
+            jedis.select(1);
+
             jedis.setex(key,time,key);
             JedisUtil.returnResource(jedis);
+        }
+
+    }
+
+    public static void doSend(String id){
+        Runnable runnable = sendMessageMap.get(id);
+        if(switchMap.get(id)){
+            executor.execute(runnable);
         }
     }
 }

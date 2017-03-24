c78a0b1f1020a3d38ce8862b68940f31bce973ea
t912772863 <912772863@qq.com>
2017-03-24 15:32:33 +0800

    把之前的svn干掉,提交到git上

---
+++ springmvcmybatis-remote/src/main/java/com/tian/springmvcmybatis/remote/ITestRemoteService.java
@@ -0,0 +1,9 @@
+package com.tian.springmvcmybatis.remote;
+
+/**
+ * 测试远程接口
+ * Created by Administrator on 2017/3/22 0022.
+ */
+public interface ITestRemoteService {
+    String print(String str);
+}

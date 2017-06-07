3cf70b781d51ceb10e039311cacec369d5476143
t912772863 <912772863@qq.com>
2017-06-07 16:19:15 +0800

    ..........................

--- springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/common/LogAspect.java
+++ springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/common/LogAspect.java
@@ -26,32 +26,32 @@ import java.util.*;
 public class LogAspect {
     private final Logger logger = Logger.getLogger(LogAspect.class);
 
-//    @Before(value = "execution(* com..controller..*.*(..))")
-    public void validate(JoinPoint jp) throws Exception{
+    //    @Before(value = "execution(* com..controller..*.*(..))")
+    public void validate(JoinPoint jp) throws Exception {
         //获取参数的值
         Object[] args = jp.getArgs();
         // 获取当前拦截到方法对象
-        MethodSignature methodSignature = (MethodSignature)jp.getSignature();
+        MethodSignature methodSignature = (MethodSignature) jp.getSignature();
         Method targetMethod = methodSignature.getMethod();
-        Map<Annotation[],Object> map = new HashMap<Annotation[],Object>();
+        Map<Annotation[], Object> map = new HashMap<Annotation[], Object>();
         //注解二阶数组
         Annotation[][] annotationss = targetMethod.getParameterAnnotations();
         for (int i = 0; i < annotationss.length; i++) {
-            map.put(annotationss[i],args[i]);
+            map.put(annotationss[i], args[i]);
         }
-        for(Annotation[] a : map.keySet()){
+        for (Annotation[] a : map.keySet()) {
             //查看是否有注解,没有注解,不用验证
-            if(a == null || a.length == 0){
+            if (a == null || a.length == 0) {
                 continue;
             }
             // 有注解,按照规则进行一个个解析,看看是否通过
             for (int i = 0; i < a.length; i++) {
-                boolean b = Validate.verify(a[i],map.get(a));
-                if(!b){
-                    logger.error(map.get(a)+"  校验不通过");
-                    throw new BusinessException(500,"参数: "+map.get(a)+" 校验不通过!");
+                boolean b = Validate.verify(a[i], map.get(a));
+                if (!b) {
+                    logger.error(map.get(a) + "  校验不通过");
+                    throw new BusinessException(500, "参数: " + map.get(a) + " 校验不通过!");
                 }
-                logger.debug(map.get(a)+"  校验通过");
+                logger.debug(map.get(a) + "  校验通过");
             }
         }
 
@@ -69,12 +69,12 @@ public class LogAspect {
         StringBuffer params = new StringBuffer();
         String oneParam;
         for (Object o : jp.getArgs()) {
-            if(o== null){
+            if (o == null) {
                 continue;
             }
-            if(o instanceof Integer || o instanceof Long || o instanceof Double || o instanceof String || o instanceof Boolean ){
+            if (o instanceof Integer || o instanceof Long || o instanceof Double || o instanceof String || o instanceof Boolean) {
                 oneParam = o + "";
-            }else {
+            } else {
                 oneParam = o.toString();
             }
             params.append(oneParam + " ; ");
@@ -85,37 +85,46 @@ public class LogAspect {
 
     private void checkLogin() {
         // 获取httpServletRequest对象
-        HttpServletRequest request ;
+        HttpServletRequest request;
         ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        if(servletRequestAttributes == null ){
+        if (servletRequestAttributes == null) {
             logger.info("非页面请求,没有request对象,不拦截");
             return;
         }
         request = servletRequestAttributes.getRequest();
         String uri = request.getRequestURI();
         // ,如果本次方法就是登录方法则,直接放行
-        if(uri.contains("login")){
+        if (uri.contains("login")) {
             return;
         }
-        if(request.getSession(true).getAttribute("user")==null){
-            // 为什么这里的异常会返回给页面,
-//            throw new BusinessException(501,"请先登录");
-
-            // 重定向到登录页面
+        if (request.getSession(true).getAttribute("user") == null) {
             try {
                 HttpServletResponse response = servletRequestAttributes.getResponse();
+                // TODO: 2017/3/25 0025 这里验证token?
+                Object token = request.getSession(true).getAttribute("token");
+                if (token != null) {
+                    //验证token是否合法
+                    response.sendRedirect("http://localhost:8086/login/check_token?token="+token.toString());
+                    return;
+                }
+
+                // 重定向到登录页面
+
+
                 // 项目配置的访问路径
-                String s =request.getContextPath();
+                String s = request.getContextPath();
                 // 当前请求的访问路径,包括了项目访问路径,但是不包括ip和端口号
                 String s2 = request.getRequestURI();
                 // 本次请求的完整路径,也就是浏览器地址栏中看到的.
                 String s3 = request.getRequestURL().toString();
 
                 // 处理上面几个路径,得到项目启动默认访问的路径,一般为登录页面.
-                response.sendRedirect(s3.replace(s2,"")+s);
+//                response.sendRedirect(s3.replace(s2,"")+s);
+                response.sendRedirect("http://localhost:8086/login/check_login?url=" + s3);
+                throw new BusinessException(501, "请先登录");
             } catch (IOException e) {
                 e.printStackTrace();
-                throw new BusinessException(501,"请先登录");
+                throw new BusinessException(501, "请先登录");
             }
         }
     }
@@ -125,6 +134,7 @@ public class LogAspect {
      * 环绕通过会拦截两次,所以换成了前置拦截+后置拦截,来实现日志.
      * 且因为如果同时用了切面和spring的拦截器,会导致,切面中会拦截到多次spring底层的方法,所以,
      * 现在登录验证想办法也通过拦截器来实现,至于出现问题的原因,和解决办法,还有待解决
+     *
      * @return
      * @throws Throwable
      */
@@ -150,10 +160,9 @@ public class LogAspect {
 //        logger.info("====> result : "+result.toString());
 //        return result;
 //    }
-
-    @AfterReturning(returning="result", value = "execution(* com.tian.springmvcmybatis.controller..*.*(..))")
+    @AfterReturning(returning = "result", value = "execution(* com.tian.springmvcmybatis.controller..*.*(..))")
     public void after(Object result) throws Throwable {
-        logger.info("====> result : "+(result==null?"null":result.toString()));
+        logger.info("====> result : " + (result == null ? "null" : result.toString()));
     }
 
 }
--- springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/common/LoginHandlerInterceptor.java
+++ springmvcmybatis-controller/src/main/java/com/tian/springmvcmybatis/controller/common/LoginHandlerInterceptor.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
+ * 登录拦截器, 已经弃用, 改用切面实现,对所有调用controller的方法都拦截,进行是否登录的检验
  * Created by Administrator on 2016/11/30 0030.
  */
 public class LoginHandlerInterceptor implements HandlerInterceptor {

31197b6fb71644dfa8ef5f7b0806474fa1d60c17
t912772863 <912772863@qq.com>
2017-08-08 14:16:52 +0800

    反射相关知识的补充

--- springmvcmybatis-controller/src/test/java/reflex/ReflexDemo01.java
+++ springmvcmybatis-controller/src/test/java/reflex/ReflexDemo01.java
@@ -9,8 +9,23 @@ import java.lang.reflect.Method;
 public class ReflexDemo01 {
     public static void main(String[] args) {
         ReflexDemo01 reflexDemo = new ReflexDemo01();
-        // 属性获取
+        // 属性获取, 该方法,只能获取到非private的属性, 比如本示例中的test属性.
         Field[] fields = ReflexDemo01.class.getFields();
+        // 该方法可以获取到所有的属性,包括private
+        Field[] fields2 = ReflexDemo01.class.getDeclaredFields();
+
+        Object o = new ReflexDemo01();
+        // 这里用Object类型来包装了ReflexDemo01,但是在通过反射获取属性的时候,还是可以获取到ReflexDemo01的属性
+        Field[] fields3 = o.getClass().getDeclaredFields();
+        for(Field f : fields3){
+            // 属性名
+            System.out.println(f.getName());
+            // 属性的类型
+            System.out.println(f.getGenericType().toString());
+        }
+
+
+
         // 方法获取
         Method[] methods = ReflexDemo01.class.getMethods();
         for (Method m : methods) {
@@ -31,6 +46,15 @@ public class ReflexDemo01 {
 
     private int age;
     private String name;
+    public String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
 
     public int getAge() {
         return age;

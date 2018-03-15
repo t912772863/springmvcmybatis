f0760c7d1537422af5b4ec7e6b19a283d3d84be6
t912772863 <912772863@qq.com>
2018-03-15 09:25:33 +0800

    .......

--- springmvcmybatis-controller/src/test/java/multithread/ThreadInfo.java
+++ springmvcmybatis-controller/src/test/java/multithread/ThreadInfo.java
@@ -1,7 +1,19 @@
 package multithread;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
 /**
  * Created by Administrator on 2018/2/28 0028.
  */
 public class ThreadInfo {
+    public static void main(String[] args) {
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        java.lang.management.ThreadInfo[] threadInfos = threadMXBean.dumpAllThreads(false,false);
+        for (java.lang.management.ThreadInfo t:threadInfos
+             ) {
+            System.out.println("thread id: "+t.getThreadId()+";  thread name: "+t.getThreadName());
+        }
+
+    }
 }
--- springmvcmybatis-controller/src/test/java/temp/MylambdaInterface.java
+++ springmvcmybatis-controller/src/test/java/temp/MylambdaInterface.java
@@ -3,5 +3,7 @@ package temp;
 /**
  * Created by Administrator on 2018/3/2 0002.
  */
-public class MylambdaInterface {
+@FunctionalInterface
+public interface MylambdaInterface {
+    void doSomeThing(String s);
 }

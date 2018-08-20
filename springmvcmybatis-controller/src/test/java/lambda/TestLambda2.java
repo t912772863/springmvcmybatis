c15aec700cbf11d3867678b6589b1adfd25c213f
t912772863 <912772863@qq.com>
2018-08-20 13:34:10 +0800

    lambda表达式学习笔记

---
+++ springmvcmybatis-controller/src/test/java/lambda/TestLambda2.java
@@ -0,0 +1,12 @@
+package lambda;
+
+/**
+ * Created by Administrator on 2018/8/10 0010.
+ */
+public class TestLambda2 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
--- springmvcmybatis-controller/src/test/java/temp/Demo003.java
+++ springmvcmybatis-controller/src/test/java/temp/Demo003.java
@@ -1,15 +1,25 @@
 package temp;
 
 
+import java.io.*;
+
 /**
  * 动态规划
  */
 public class Demo003 {
 
-    public static void main(String[] args) {
-        System.out.println(Runtime.getRuntime().availableProcessors());
+    public static void main(String[] args) throws IOException {
+        String path = "E:\\临时文件夹\\300w.txt";
+        File f = new File(path);
+        OutputStream out = new FileOutputStream(f);
+        long phone = 13500000000L;
+        for (int i = 0; i < 3000000; i++) {
+            String lineText = ""+phone+", 13459,AQWE4,wwaddfff,AAAAAA3,123458,020-88888890,7月9日,11111111.59"+"\r\n";
+            phone++;
+            out.write(lineText.getBytes());
 
-
+        }
+        out.close();
     }
 
 
